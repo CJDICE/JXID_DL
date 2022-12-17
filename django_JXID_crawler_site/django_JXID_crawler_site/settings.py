61355467af92247d1a7739f72a6da0cd5b9d7df5
CJDICE <icechiu@aetheras.io>
2022-12-18 01:25:20 +0800

    Revising secret key issue

--- django_JXID_crawler_site/django_JXID_crawler_site/settings.py
+++ django_JXID_crawler_site/django_JXID_crawler_site/settings.py
@@ -19,9 +19,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-1(*i)$)eym!mj37&(4-a18r#8!-qj@(=qng=4zr$t+o$2e6%di'
-
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
